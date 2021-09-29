4d0d0f1877e1f1a56b865f14041149beb6527a9e
SilanSurfer <szwarc.adam@gmail.com>
2021-09-30 01:08:46 +0200

    feat: Added iteration of lines using tokio_stream
    Lines from reader are now iterated using async feature tokio_stream.
    Moved main loop to separate function run(...).

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::path::PathBuf;
 use structopt::StructOpt;
+use tokio_stream::StreamExt;
 
 mod analyzer;
 mod cli;
@@ -12,22 +14,31 @@ use crate::analyzer::TypeAnalyzer;
 use crate::cli::CliArgs;
 use crate::error::AppError;
 
+async fn run(input_file_path: PathBuf) -> Result<(), AppError> {
+    log::info!("Analysing file: {:?}", input_file_path);
+
+    // TODO: This could be async
+    let file = File::open(input_file_path)?;
+    let reader = BufReader::new(file);
+    let mut analyzer = TypeAnalyzer::new();
+
+    let mut stream = tokio_stream::iter(reader.lines().enumerate());
+    while let Some((no, line)) = stream.next().await {
+        let _res = analyzer
+            .check_line(line?)
+            .await
+            .map_err(|err| log::warn!("Error while reading line no {}: {}", no, err));
+    }
+
+    log::info!("Final result:\n");
+    analyzer.print_output();
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), AppError> {
     let args = CliArgs::from_args();
     logger::configure_logger(args.verbose);
-    log::info!("Analysing file: {:?}", args.input_file_path);
-    let file = File::open(args.input_file_path)?;
-    let reader = BufReader::new(file);
-    let mut analyzer = TypeAnalyzer::new();
 
-    for line in reader.lines() {
-        let line = line?;
-        if let Err(e) = analyzer.check_line(line).await {
-            log::warn!("Error while reading line: {}", e);
-        }
-    }
-
-    analyzer.print_output();
-    Ok(())
+    run(args.input_file_path).await
 }
